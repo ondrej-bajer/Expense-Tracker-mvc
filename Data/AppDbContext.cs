7f587c48a9ff4511074a3ba25156d689dd23c10e
Ondřej Bajer <bajer.o@centrum.cz>
2026-02-21 16:28:41 +0100

    feat: add FxRates and FxRateFetchLogs to DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,5 +10,31 @@ namespace Expense_Tracker_mvc.Data
 
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<TransactionCategory> TransactionCategories { get; set; }
+
+        public DbSet<FxRate> FxRates { get; set; }
+        public DbSet<FxRateFetchLog> FxRateFetchLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<FxRate>()
+                .HasIndex(x => new { x.Date, x.Code })
+                .IsUnique();
+
+            modelBuilder.Entity<FxRate>()
+                .Property(x => x.Code)
+                .HasMaxLength(3)
+                .IsRequired();
+
+            modelBuilder.Entity<FxRateFetchLog>()
+                .HasIndex(x => x.RequestedDate)
+                .IsUnique();
+
+            modelBuilder.Entity<FxRateFetchLog>()
+                .Property(x => x.Status)
+                .HasMaxLength(16)
+                .IsRequired();
+        }
     }
 }
